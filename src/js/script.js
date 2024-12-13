60dcf87c6343d984b44b0f7bba1733f98f83af9e
alice-frln <alice.frelin@edu.univ-fcomte.fr>
2024-12-13 11:21:42 +0100

    menu et autres

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const page = document.body;
+
+// Vérifier si les éléments existent avant d'ajouter l'événement
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    // Mise à jour des attributs ARIA pour accessibilité
+    toggle.ariaExpanded = isClosed;
+    nav.ariaHidden = isOpen;
+    page.classList.toggle("noscroll", isClosed);
+  });
+}
+
+
+
+
+//Carousel\\
 const track = document.querySelector('.carousel-track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel-button.next');
@@ -19,3 +39,4 @@ prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
     updateSlidePosition();
 });
+
